72d23deb8fcd4bf9e65fea7256326891205f8802
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2025-11-07 20:32:27 +0600

    Add Render to ALLOWED_HOSTS

--- ByteBank/settings.py
+++ ByteBank/settings.py
@@ -30,7 +30,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['byte-bank.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
